cc557fcdd3fec2cb67efeb1f875b4d7d9d85b5b4
MoCuishle28 <20164706@s.hlju.edu.cn>
2019-01-19 19:53:15 +0800

    preliminary understanding django

--- learn-django/mysite/polls/admin.py
+++ learn-django/mysite/polls/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from .models import Question, Choice
 
 
-# Register your models here.
-admin.site.register(Question)
+# Register your models here. 注册完就能在管理页面看到数据
+
+# 可以重排列管理员界面表单的字段的顺序
+class QuestionAdmin(admin.ModelAdmin):
+	fields = ['pub_date', 'question_text']
+
+# admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
